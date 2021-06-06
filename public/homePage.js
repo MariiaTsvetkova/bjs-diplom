b0202f62e41f6533c0a51139826b647cd4e5b23f
MariiaTsvetkova <65927414+MariiaTsvetkova@users.noreply.github.com>
2021-06-06 23:29:57 +0300

    Update homePage.js

---
+++ public/homePage.js
@@ -0,0 +1,89 @@
+
+
+let logoutButton = new LogoutButton();
+logoutButton.action = () => {
+    ApiConnector.logout(response => {
+        if (response.success)
+            location.reload();
+    })
+};
+
+
+ApiConnector.current((response) => {
+    if (response.success) {
+        ProfileWidget.showProfile(response.data);
+    }
+});
+
+let ratesBoard = new RatesBoard();
+
+ApiConnector.getStocks(response => {
+    if (response.success) {
+        ratesBoard.clearTable();
+        ratesBoard.fillTable(response.data);
+    }
+})
+
+let moneyManager = new MoneyManager();
+
+moneyManager.addMoneyCallback = data => {
+    ApiConnector.addMoney(data, response => {
+        if (response.success) {
+            ProfileWidget.showProfile(response.data);
+        }
+
+        moneyManager.setMessage(response.success, response.error);
+    })
+}
+
+moneyManager.conversionMoneyCallback = data => {
+    ApiConnector.convertMoney(data, response => {
+        if (response.success) {
+            ProfileWidget.showProfile(response.data);
+        }
+
+        moneyManager.setMessage(response.success, response.error);
+    })
+}
+
+moneyManager.sendMoneyCallback = data => {
+    ApiConnector.transferMoney(data, response => {
+        if (response.success) {
+            ProfileWidget.showProfile(response.data);
+        }
+
+        moneyManager.setMessage(response.success, response.error);
+    })
+}
+
+let favoritesWidget = new FavoritesWidget();
+
+ApiConnector.getFavorites(response => {
+    if (response.success) {
+        favoritesWidget.clearTable(response.data);
+        favoritesWidget.fillTable(response.data);
+        moneyManager.updateUsersList(response.data);
+    }
+});
+
+favoritesWidget.addUserCallback = data => {
+    ApiConnector.addUserToFavorites(data, response => {
+        if (response.success) {
+            favoritesWidget.clearTable(response.data);
+            favoritesWidget.fillTable(response.data);
+            moneyManager.updateUsersList(response.data);
+        }
+        favoritesWidget.setMessage(response.success, response.error);
+    })
+}
+
+favoritesWidget.removeUserCallback = data => {
+    ApiConnector.removeUserFromFavorites(data, response => {
+        if (response.success) {
+            favoritesWidget.clearTable(response.data);
+            favoritesWidget.fillTable(response.data);
+            moneyManager.updateUsersList(response.data);
+        }
+        favoritesWidget.setMessage(response.success, response.error);
+    })
+}
